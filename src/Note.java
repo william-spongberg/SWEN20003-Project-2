61875a58b90c7e8b88bb48b1ff488bd710866ba7
william-spongberg <william.spongberg@outlook.com>
2023-10-02 14:59:12 +1100

    Now as an interface allows multiple types of notes

--- src/Note.java
+++ src/Note.java
@@ -1,181 +1,55 @@
 import bagel.*;
-
-public class Note {
-    // images
-    private final Image IMAGE_NOTE_LEFT = new Image("res/noteLeft.png");
-    private final Image IMAGE_NOTE_RIGHT = new Image("res/noteRight.png");
-    private final Image IMAGE_NOTE_UP = new Image("res/noteUp.png");
-    private final Image IMAGE_NOTE_DOWN = new Image("res/noteDown.png");
-
-    private final Image IMAGE_NOTEHOLD_LEFT = new Image("res/holdNoteLeft.png");
-    private final Image IMAGE_NOTEHOLD_RIGHT = new Image("res/holdNoteRight.png");
-    private final Image IMAGE_NOTEHOLD_UP = new Image("res/holdNoteUp.png");
-    private final Image IMAGE_NOTEHOLD_DOWN = new Image("res/holdNoteDown.png");
-
+public interface Note {
     // directions
-    private static final int LEFT = 0;
-    private static final int RIGHT = 1;
-    private static final int UP = 2;
-    private static final int DOWN = 3;
+    public static final int LEFT = 0;
+    public static final int RIGHT = 1;
+    public static final int UP = 2;
+    public static final int DOWN = 3;
+    public static final int SPECIAL = 4;
 
-    // starting y position
-    private static final int START_Y = 100;
-    private static final int START_Y_HOLD = 24;
+    // types
+    public static final int NORMAL = 0;
+    public static final int HOLD = 1;
+    public static final int DOUBLE_SCORE = 2;
+    public static final int BOMB = 3;
+    public static final int SPEED_UP = 4;
+    public static final int SLOW_DOWN = 5;
+
+    public static final String TYPE_LEFT = "Left";
+    public static final String TYPE_RIGHT = "Right";
+    public static final String TYPE_UP = "Up";
+    public static final String TYPE_DOWN = "Down";
+    public static final String TYPE_SPECIAL = "Special";
+    public static final String TYPE_HOLD = "Hold";
+    public static final String TYPE_DOUBLE_SCORE = "DoubleScore";
+    public static final String TYPE_BOMB = "Bomb";
+    public static final String TYPE_SPEED_UP = "SpeedUp";
+    public static final String TYPE_SLOW_DOWN = "SlowDown";
 
     // refresh rate speed multipliers
-    private static final int REFRESH_60_MULTIPLIER = 4;
-    private static final int REFRESH_120_MULTIPLIER = 2;
+    public static final int REFRESH_60_MULTIPLIER = 4;
+    public static final int REFRESH_120_MULTIPLIER = 2;
 
-    // hold image midpoint
-    private static final int HOLD_MIDPOINT = 82;
-
-    // attributes with default values
-    private Image image = IMAGE_NOTEHOLD_LEFT;
-    private int delay = 0;
-    private int dir = LEFT;
-    private int y = 0;
-    private int start_y = START_Y;
-    private boolean is_held = false;
-    private boolean active = true;
-    private boolean visual = false;
-    private boolean below_screen = false;
-
-    public Note(String dir, String type, int delay) {
-        reset(dir, type, delay);
-    }
-
-    public void reset(String dir, String type, int delay) {
-        // if held set to held
-        if (type.equals("Hold")) {
-            this.is_held = true;
-            start_y = START_Y_HOLD;
-        }
-
-        // set image direction, note direction to dir
-        switch (dir) {
-            case "Left":
-                if (this.is_held)
-                    this.image = IMAGE_NOTEHOLD_LEFT;
-                else
-                    this.image = IMAGE_NOTE_LEFT;
-                this.dir = LEFT;
-                break;
-            case "Right":
-                if (this.is_held)
-                    this.image = IMAGE_NOTEHOLD_RIGHT;
-                else
-                    this.image = IMAGE_NOTE_RIGHT;
-                this.dir = RIGHT;
-                break;
-            case "Up":
-                if (this.is_held)
-                    this.image = IMAGE_NOTEHOLD_UP;
-                else
-                    this.image = IMAGE_NOTE_UP;
-                this.dir = UP;
-                break;
-            case "Down":
-                if (this.is_held)
-                    this.image = IMAGE_NOTEHOLD_DOWN;
-                else
-                    this.image = IMAGE_NOTE_DOWN;
-                this.dir = DOWN;
-                break;
-            default:
-                System.out.println("Error: invalid note");
-        }
-        // set note delay
-        this.delay = delay;
-    }
-
-    // reset to default values
-    // but keep original direction and delay
-    public void reset(Note note) {
-        this.image = note.getImage();
-        this.delay = note.getDelay();
-        this.dir = note.getDir();
-        this.is_held = note.isHeld();
-        this.y = 0;
-        this.start_y = note.start_y;
-
-        this.active = true;
-        this.visual = false;
-        this.below_screen = false;
-    }
-
-    // update note position
-    public void update(int frame, int lane_x[]) {
-        // if active or visual
-        if (this.active || this.visual) {
-            // calculate y position
-            this.y = start_y + (frame - this.delay) * REFRESH_60_MULTIPLIER;
-                    // * ((ShadowDance.hasRefresh60()) ? REFRESH_60_MULTIPLIER : REFRESH_120_MULTIPLIER);
-
-            // if note is on screen
-            if ((this.y > start_y) && (this.y < (ShadowDance.getHeight() + this.image.getHeight() / 2))) {
-                // now visual
-                if (!this.visual)
-                    this.visual = true;
-
-                // draw note
-                this.image.draw(lane_x[this.dir], this.y);
-            }
-            // if note is below screen
-            else if (this.y > (ShadowDance.getHeight() + this.image.getHeight() / 2)) {
-                this.visual = false;
-                this.active = false;
-                this.below_screen = true;
-            }
-        }
-    }
+    // methods
+    public void reset(Note note);
+    public void reset(String dir, String type, int delay);
+    public void update(int frame, int lane_x[]);
+    public Boolean canGrade(Input input);
 
     /* getters */
-    public Image getImage() {
-        return this.image;
-    }
-
-    public Integer getDir() {
-        return this.dir;
-    }
-
-    public Integer getDelay() {
-        return this.delay;
-    }
-
-    public Integer getY() {
-        return this.y;
-    }
-
-    public Boolean isHeld() {
-        return this.is_held;
-    }
-
-    public Boolean isActive() {
-        return this.active;
-    }
-
-    public Boolean isVisual() {
-        return this.visual;
-    }
-
-    public Boolean isBelowScreen() {
-        return this.below_screen;
-    }
-
-    public int getHeldMidpoint() {
-        return HOLD_MIDPOINT;
-    }
+    public Image getImage();    
+    public Integer getDir();
+    public Integer getType();
+    public Integer getDelay();
+    public Double getY();
+    public Double getStartY();
+    public Integer getMidpoint();
+    public Boolean isActive();
+    public Boolean isVisual();
+    public Boolean isBelowScreen();
 
     /* setters */
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
-
-    public void setVisual(Boolean visual) {
-        this.visual = visual;
-    }
-
-    public void setBelowScreen(Boolean below_screen) {
-        this.below_screen = below_screen;
-    }
+    public void setActive(Boolean active);
+    public void setVisual(Boolean visual);
+    public void setBelowScreen(Boolean below_screen);
 }
