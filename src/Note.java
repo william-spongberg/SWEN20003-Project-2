da12e964437a9d23062e723192ae2303be9fa036
william-spongberg <william.spongberg@outlook.com>
2023-10-04 20:06:34 +1100

    fixing formatting & variable names

--- src/Note.java
+++ src/Note.java
@@ -1,11 +1,18 @@
-import bagel.*;
+import bagel.Image;
 public interface Note {
     // directions
+    public static final int NULL = -1;
     public static final int LEFT = 0;
     public static final int RIGHT = 1;
     public static final int UP = 2;
     public static final int DOWN = 3;
     public static final int SPECIAL = 4;
+
+    public static final String STR_LEFT = "Left";
+    public static final String STR_RIGHT = "Right";
+    public static final String STR_UP = "Up";
+    public static final String STR_DOWN = "Down";
+    public static final String STR_SPECIAL = "Special";
 
     // types
     public static final int NORMAL = 0;
@@ -15,16 +22,12 @@ public interface Note {
     public static final int SPEED_UP = 4;
     public static final int SLOW_DOWN = 5;
 
-    public static final String TYPE_LEFT = "Left";
-    public static final String TYPE_RIGHT = "Right";
-    public static final String TYPE_UP = "Up";
-    public static final String TYPE_DOWN = "Down";
-    public static final String TYPE_SPECIAL = "Special";
-    public static final String TYPE_HOLD = "Hold";
-    public static final String TYPE_DOUBLE_SCORE = "DoubleScore";
-    public static final String TYPE_BOMB = "Bomb";
-    public static final String TYPE_SPEED_UP = "SpeedUp";
-    public static final String TYPE_SLOW_DOWN = "SlowDown";
+    public static final String STR_NORMAL = "Normal";
+    public static final String STR_HOLD = "Hold";
+    public static final String STR_DOUBLE_SCORE = "DoubleScore";
+    public static final String STR_BOMB = "Bomb";
+    public static final String STR_SPEED_UP = "SpeedUp";
+    public static final String STR_SLOW_DOWN = "SlowDown";
 
     // refresh rate speed multipliers
     public static final int REFRESH_60_MULTIPLIER = 4;
@@ -34,7 +37,6 @@ public interface Note {
     public void reset(Note note);
     public void reset(String dir, String type, int delay);
     public void update(int frame, int lane_x[]);
-    public Boolean canGrade(Input input);
 
     /* getters */
     public Image getImage();    
@@ -43,6 +45,7 @@ public interface Note {
     public Integer getDelay();
     public Double getY();
     public Double getStartY();
+    public Integer getSpeed();
     public Integer getMidpoint();
     public Boolean isActive();
     public Boolean isVisual();
@@ -52,4 +55,5 @@ public interface Note {
     public void setActive(Boolean active);
     public void setVisual(Boolean visual);
     public void setBelowScreen(Boolean below_screen);
+    public void setSpeed(Integer speed);
 }
