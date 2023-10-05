44cfa4d30a7280073ae3a1826590011cd5e5538d
william-spongberg <william.spongberg@outlook.com>
2023-10-05 12:15:37 +1100

    now allowed multiple notes graded at once, bug fix

--- src/Grader.java
+++ src/Grader.java
@@ -2,18 +2,19 @@ import bagel.Input;
 import bagel.Keys;
 
 public class Grader {
+    // TODO: change constants to public, avoid getters here
     // grading
-    private static final int SPECIAL = 0;
-    private static final int PERFECT = 1;
-    private static final int GOOD = 2;
-    private static final int BAD = 3;
-    private static final int MISS = 4;
+    private static final int PERFECT = 0;
+    private static final int GOOD = 1;
+    private static final int BAD = 2;
+    private static final int MISS = 3;
+    private static final int SPECIAL = 4;
 
     // grading scores
-    private static final int[] GRADE = { 15, 10, 5, -1, -5 };
+    private static final int[] GRADE = { 10, 5, -1, -5, 15 };
 
     // grading distances
-    private static final int[] DISTANCE = { 50, 15, 50, 100, 200 };
+    private static final int[] DISTANCE = { 15, 50, 100, 200, 50 };
 
     // grade y position
     private static final int NOTE_STATIONARY_Y = 657;
@@ -23,38 +24,37 @@ public class Grader {
     private boolean specialGrade = false;
 
     // get score from player input
-    public Boolean[] checkScore(Note note, Input input, Boolean holding) {
+    public Boolean[] checkScore(final Note note, final Input input, final int lane_dir, boolean holding) {
+        // reset values
+        this.grade = 0;
+        this.specialGrade = false;
+
         // input, note_y set to defualt
-        int dir = Note.NULL;
+        boolean dir[] = new boolean[5];
         double note_y = note.getY();
 
-        // TODO: combine normal/special type checks here
-        // if note is normal
-        if (note.getType() == Note.NORMAL) {
+        // if note is normal/special
+        if (note.getType() != Note.HOLD) {
             dir = checkInputPressed(input);
         }
-        // if note is special
-        else if (note.getType() > Note.HOLD) {
-            dir = checkInputPressed(input);
-        }
-        // else if note is held and not already holding
-        else if (note.getType() == Note.HOLD && !holding) {
-            dir = checkInputPressed(input);
-            // if valid input
-            if (dir != Note.NULL) {
-                // get bottom of held note, now holding
-                note_y = note.getY() + note.getMidpoint();
-                holding = true;
-            }
-        }
-        // else if note is held and already holding
-        else if (note.getType() == Note.HOLD && holding) {
-            dir = checkInputReleased(input);
-            // if valid input
-            if (dir != Note.NULL) {
-                // get top of held note, now not holding
-                note_y = note.getY() - note.getMidpoint();
-                holding = false;
+        // else if note is held
+        else {
+            if (!holding) {
+                // check press, correct dir
+                dir = checkInputPressed(input);
+                if (dir[lane_dir]) {
+                    // get bottom of held note, now holding
+                    note_y = note.getY() + note.getMidpoint();
+                    holding = true;
+                }
+            } else {
+                // check release, correct dir
+                dir = checkInputReleased(input);
+                if (dir[lane_dir]) {
+                    // get top of held note, now not holding
+                    note_y = note.getY() - note.getMidpoint();
+                    holding = false;
+                }
             }
         }
 
@@ -62,16 +62,19 @@ public class Grader {
         if ((NOTE_STATIONARY_Y - note_y <= DISTANCE[MISS])
                 && (((NOTE_STATIONARY_Y - note_y >= 0) && note.getType() != Note.HOLD)
                         || ((NOTE_STATIONARY_Y - note_y + note.getMidpoint() >= 0) && note.getType() == Note.HOLD))) {
-            if (note.getType() == Note.HOLD || note.getType() == Note.NORMAL) {
-                // grade the input, kill note if graded and not holding
-                if (gradeInput(dir, note, note_y)) {
-                    if (!holding || this.grade == GRADE[MISS])
-                        note.setActive(false);
-                }
-            }
-            else if (note.getType() > Note.HOLD) {
-                // grade the input, kill note if graded
-                if (gradeSpecialInput(dir, note, note_y)) {
+            // check correct dir
+            if (dir[lane_dir]) {
+                // if normal or hold
+                if (note.getType() == Note.NORMAL || note.getType() == Note.HOLD) {
+                    // grade the input, kill note if graded and not holding
+                    if (gradeInput(dir, note, note_y)) {
+                        if (!holding || this.grade == GRADE[MISS])
+                            note.setActive(false);
+                    }
+                    // if special
+                } else if (note.getType() > Note.HOLD) {
+                    // grade the input, kill note if graded
+                    if (gradeSpecialInput(dir, note, note_y))
                         note.setActive(false);
                 }
             }
@@ -80,16 +83,17 @@ public class Grader {
         else if (!(NOTE_STATIONARY_Y - note_y >= 0)) {
             note.setActive(false);
         }
-        return new Boolean[] { note.isActive(), holding };
+
+        return new Boolean[] {note.isActive(), holding };
     }
 
     // grade player input
-    public Boolean gradeInput(int dir, Note note, double note_y) {
+    public Boolean gradeInput(final boolean dir[], final Note note, final double note_y) {
         boolean graded = false;
-        // if valid input and note is on screen
-        if (dir != Note.NULL && note_y <= NOTE_STATIONARY_Y) {
+        // if note is on screen
+        if (note_y <= NOTE_STATIONARY_Y) {
             // if wrong direction
-            if (dir != note.getDir()) {
+            if (!dir[note.getDir()]) {
                 this.grade = GRADE[MISS];
             } else {
                 // if within range
@@ -104,19 +108,23 @@ public class Grader {
     }
 
     // grade player input for special note
-    public Boolean gradeSpecialInput(int dir, Note note, double note_y) {
+    public Boolean gradeSpecialInput(final boolean dir[], final Note note, final double note_y) {
         boolean graded = false;
         // if valid input and note is on screen
-        if (dir != Note.NULL && note_y <= NOTE_STATIONARY_Y) {
+        if (note_y <= NOTE_STATIONARY_Y) {
             // if wrong direction
-            if (dir != note.getDir()) {
-                this.grade = GRADE[MISS];
+            if (!dir[note.getDir()]) {
+                // this.grade = GRADE[MISS];
             } else {
                 // if within range
                 if (graded = gradeDistance(SPECIAL, note, note_y)) {
                     this.specialGrade = true;
+
+                    /* testing */
+                    System.out.println("grade: " + this.grade + " special: " + this.specialGrade);
+                    System.out.println(
+                            "dir: " + note.getDir() + " type: " + note.getType() + " delay: " + note.getDelay());
                 }
-                // else do nothing
             }
         }
         return graded;
@@ -124,7 +132,7 @@ public class Grader {
 
     // grade player input for range grade
     // and set note to graded + not visual
-    public Boolean gradeDistance(int grade, Note note, double note_y) {
+    public Boolean gradeDistance(final int grade, final Note note, final double note_y) {
         // if in grade range
         if (NOTE_STATIONARY_Y - note_y <= DISTANCE[grade]) {
             this.grade = GRADE[grade];
@@ -134,38 +142,36 @@ public class Grader {
         return false;
     }
 
-    // check for key released
-    public Integer checkInputReleased(Input input) {
-        if (input.wasReleased(Keys.LEFT)) {
-            return Note.LEFT;
-        } else if (input.wasReleased(Keys.RIGHT)) {
-            return Note.RIGHT;
-        } else if (input.wasReleased(Keys.UP)) {
-            return Note.UP;
-        } else if (input.wasReleased(Keys.DOWN)) {
-            return Note.DOWN;
-        } else if (input.wasPressed(Keys.SPACE)) {
-            return Note.SPECIAL;
-        } else {
-            return Note.NULL;
-        }
+    // check for key pressed
+    public boolean[] checkInputPressed(final Input input) {
+        final boolean dir[] = { false, false, false, false, false };
+        if (input.wasPressed(Keys.LEFT))
+            dir[Note.LEFT] = true;
+        if (input.wasPressed(Keys.RIGHT))
+            dir[Note.RIGHT] = true;
+        if (input.wasPressed(Keys.UP))
+            dir[Note.UP] = true;
+        if (input.wasPressed(Keys.DOWN))
+            dir[Note.DOWN] = true;
+        if (input.wasPressed(Keys.SPACE))
+            dir[Note.SPECIAL] = true;
+
+        return dir;
     }
 
-    // check for key pressed
-    public Integer checkInputPressed(Input input) {
-        if (input.wasPressed(Keys.LEFT)) {
-            return Note.LEFT;
-        } else if (input.wasPressed(Keys.RIGHT)) {
-            return Note.RIGHT;
-        } else if (input.wasPressed(Keys.UP)) {
-            return Note.UP;
-        } else if (input.wasPressed(Keys.DOWN)) {
-            return Note.DOWN;
-        } else if (input.wasPressed(Keys.SPACE)) {
-            return Note.SPECIAL;
-        } else {
-            return Note.NULL;
-        }
+    // check for key released
+    public boolean[] checkInputReleased(final Input input) {
+        final boolean dir[] = { false, false, false, false, false };
+        if (input.wasReleased(Keys.LEFT))
+            dir[Note.LEFT] = true;
+        if (input.wasReleased(Keys.RIGHT))
+            dir[Note.RIGHT] = true;
+        if (input.wasReleased(Keys.UP))
+            dir[Note.UP] = true;
+        if (input.wasReleased(Keys.DOWN))
+            dir[Note.DOWN] = true;
+
+        return dir;
     }
 
     /* getters */
